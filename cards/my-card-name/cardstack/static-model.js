0631af08a500c4b456a47d09f795e8adf337d6c4
Jen Weber <j@jenweber.me>
2019-06-25 16:04:33 -0400

    working individual card

--- cards/my-card-name/cardstack/static-model.js
+++ cards/my-card-name/cardstack/static-model.js
@@ -5,8 +5,16 @@ factory.addResource('content-types', 'my-card-names')
   .withRelated('fields', [
     factory.addResource('fields', 'title').withAttributes({
       fieldType: '@cardstack/core-types::string'
-    })
+    }),
+    factory.addResource('fields', 'nickname').withAttributes({
+      fieldType: '@cardstack/core-types::string'
+    }),
   ]);
+
+  factory.addResource('my-card-names', 1).withAttributes({
+    title: 'Wow',
+    nickname: 'testing'
+  });
 
 let models = factory.getModels();
 module.exports = function() { return models; };
